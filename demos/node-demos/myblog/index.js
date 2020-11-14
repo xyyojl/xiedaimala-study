d1fbc8240277f6e0f932690734fab6f566019ae4
xyyojl <m13631897256@163.com>
2020-11-14 22:00:39 +0800

    了解模板引擎

--- demos/node-demos/myblog/index.js
+++ demos/node-demos/myblog/index.js
@@ -1,7 +1,12 @@
+const path = require('path');
 const express = require('express');
 const app = express();
 const indexRouter = require('./routes/index');
 const userRouter = require('./routes/users');
+
+// 设置模板引擎
+app.set('views', path.join(__dirname, 'views')); // 设置存放模板文件的目录
+app.set('view engine', 'ejs'); // 设置模板引擎为 ejs
 
 app.use('/', indexRouter);
 app.use('/users', userRouter);
--- demos/node-demos/myblog/routes/users.js
+++ demos/node-demos/myblog/routes/users.js
@@ -2,7 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/:name', function (req, res) {
-    res.send('Hello, ' + req.params.name);
+    // res.send('Hello, ' + req.params.name);
+    res.render('users', {
+        name: req.params.name
+    })
 })
 
 module.exports = router;
