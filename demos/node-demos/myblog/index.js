11b3c6b0a3d2719db115538a077118b9d617179b
xyyojl <m13631897256@163.com>
2020-11-15 11:08:15 +0800

    学习中间件与 next

--- demos/node-demos/myblog/index.js
+++ demos/node-demos/myblog/index.js
@@ -5,10 +5,27 @@ const indexRouter = require('./routes/index');
 const userRouter = require('./routes/users');
 
 // 设置模板引擎
-app.set('views', path.join(__dirname, 'views')); // 设置存放模板文件的目录
+/* app.set('views', path.join(__dirname, 'views')); // 设置存放模板文件的目录
 app.set('view engine', 'ejs'); // 设置模板引擎为 ejs
 
 app.use('/', indexRouter);
-app.use('/users', userRouter);
+app.use('/users', userRouter); */
+
+
+// test
+app.use(function (req, res, next){
+    console.log('1');
+    next(new Error('haha'));
+})
+app.use(function (req, res, next) {
+    console.log('2');
+    res.status(200).end();
+})
+
+// 错误处理
+app.use(function (err, req, res, next) {
+    console.log(err.stack);
+    res.status(500).send('Something broke!')
+})
 
 app.listen(3000);
